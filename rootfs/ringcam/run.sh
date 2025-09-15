b3842168117e5de8aa80e93a940e3e52a53e43ba
Dan Grover <dan-grover@dan-grover.com>
2025-09-15 16:28:58 +0100

    More

--- rootfs/ringcam/run.sh
+++ rootfs/ringcam/run.sh
@@ -15,6 +15,8 @@ HWACCEL=$(jq -r '.hwaccel // "auto"' "$OPTIONS")
 HA_INT=$(jq -r '.ha_integration // true' "$OPTIONS")
 HA_ENTITY=$(jq -r '.ha_entity_id // "binary_sensor.ring_livestream_playing"' "$OPTIONS")
 HA_PREFIX=$(jq -r '.ha_event_prefix // "ring_livestream"' "$OPTIONS")
+HA_BASE=$(jq -r '.ha_base_url // ""' "$OPTIONS")
+HA_LL_TOKEN=$(jq -r '.ha_long_lived_token // ""' "$OPTIONS")
 
 export NODE_ENV=production
 
@@ -22,6 +24,9 @@ export NODE_ENV=production
 ARGS=(--token "$TOKEN" --name "$NAME" --quality "$QUALITY" --port "$PORT"
       --codec "$CODEC" --hwaccel "$HWACCEL"
       --ha-integration "$HA_INT" --ha-entity "$HA_ENTITY" --ha-prefix "$HA_PREFIX")
+
+[[ -n "$HA_BASE" ]] && ARGS+=(--ha-base "$HA_BASE")
+[[ -n "$HA_LL_TOKEN" ]] && ARGS+=(--ha-token "$HA_LL_TOKEN")
 [[ "$DEBUG" == "true" ]] && ARGS+=("--debug")
 
 # echo "Current value of SUPERVISOR_TOKEN is: ${SUPERVISOR_TOKEN}"
